61b98ae58dfad7a40590da07993a50e4f95c6648
Bluzume <39113159+KuromeSan@users.noreply.github.com>
2018-11-09 19:37:49 +1300

    Add files via upload

---
+++ GetPermissionToExec/MethodInvoker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GetPermissionToExec
+{
+    internal class MethodInvoker
+    {
+        private Func<object> p;
+
+        public MethodInvoker(Func<object> p)
+        {
+            this.p = p;
+        }
+    }
+}
